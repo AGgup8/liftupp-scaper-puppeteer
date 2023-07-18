c71c85f75e45da94f0e1cb3c66f703e73913e01e
AGgup8 <110463978+AGgup8@users.noreply.github.com>
2023-07-18 19:17:04 +0100

    Updated args and executable path for Puppeteer

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 
 const app = express();
 
+require("dotenv").config();
+
 app.use("/static", express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
@@ -18,7 +20,17 @@ app.get("/", (req, res) => {
 app.get("/liftupp", async (req, res) => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
+    args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote"
+    ]
     headless: "new",
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
   });
 
   try {
@@ -26,34 +38,33 @@ app.get("/liftupp", async (req, res) => {
 
     // Navigate the page to a URL
     await page.goto("https://developer.chrome.com/");
-  
+
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
-  
+
     // Type into search box
     await page.type(".search-box__input", "automate beyond recorder");
-  
+
     // Wait and click on first result
     const searchResultSelector = ".search-box__link";
     await page.waitForSelector(searchResultSelector);
     await page.click(searchResultSelector);
-  
+
     // Locate the full title with a unique string
     const textSelector = await page.waitForSelector(
       "text/Customize and automate"
     );
     const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-  
+
     // Print the full title
     console.log('The title of this blog post is "%s".', fullTitle);
     res.send(fullTitle);
   } catch (e) {
     console.error(e);
-    res.send(`Something went wrong whilst running Puppeteer: ${e}`)
+    res.send(`Something went wrong whilst running Puppeteer: ${e}`);
   } finally {
     await browser.close();
   }
-  
 });
 
 app.listen(PORT, () => {
